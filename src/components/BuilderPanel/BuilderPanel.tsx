8f957b323ac7b046773cee247e004c883253cf00
Aum Bhatt <aum@deriv.com>
2025-01-25 16:59:44 +0400

    feat: split builder panel

--- src/components/BuilderPanel/BuilderPanel.tsx
+++ src/components/BuilderPanel/BuilderPanel.tsx
@@ -1,3 +1,4 @@
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { ParametersForm } from "./components";
 import styles from "./BuilderPanel.module.scss";
 
@@ -5,7 +6,15 @@ export const BuilderPanel = () => {
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Builder Panel</h2>
-            <ParametersForm />
+            <PanelGroup direction="vertical" className={styles.panelGroup}>
+                <Panel defaultSize={60} minSize={30} className={styles.panel}>
+                    <ParametersForm />
+                </Panel>
+                <PanelResizeHandle className={styles.resizeHandle} />
+                <Panel defaultSize={40} minSize={20} className={styles.panel}>
+                    <div className={styles.bottomPanel} />
+                </Panel>
+            </PanelGroup>
         </div>
     );
 };
