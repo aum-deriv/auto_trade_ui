665b1e00f3b51d5136282c925a441d25709a7ffb
Aum Bhatt <aum@deriv.com>
2025-01-28 10:46:03 +0400

    styles: some more UI changes

--- src/components/BuilderPanel/BuilderPanel.tsx
+++ src/components/BuilderPanel/BuilderPanel.tsx
@@ -1,7 +1,7 @@
 import { Panel, PanelGroup } from "react-resizable-panels";
 import { Text } from "@deriv-com/quill-ui";
 import { ParametersForm } from "./components";
-import { ChatPanel } from "./components/ChatPanel";
+import { AIBuilder } from "./components/AIBuilder";
 import { ResizeHandle } from "../ResizeHandle";
 import styles from "./BuilderPanel.module.scss";
 
@@ -11,13 +11,14 @@ export const BuilderPanel = () => {
             <Text size="lg" bold>
                 Strategy Builder
             </Text>
+
             <PanelGroup direction="vertical" className={styles.panelGroup}>
-                <Panel defaultSize={60} minSize={30} className={styles.panel}>
+                <Panel className={styles.panel}>
                     <ParametersForm />
                 </Panel>
                 <ResizeHandle direction="vertical" />
-                <Panel defaultSize={40} minSize={20} className={styles.panel}>
-                    <ChatPanel />
+                <Panel className={styles.panel}>
+                    <AIBuilder />
                 </Panel>
             </PanelGroup>
         </div>
--- src/components/BuilderPanel/components/AIBuilder/AIBuilder.tsx
+++ src/components/BuilderPanel/components/AIBuilder/AIBuilder.tsx
@@ -1,14 +1,18 @@
 import { Text, TextField } from "@deriv-com/quill-ui";
-import styles from "./ChatPanel.module.scss";
+import styles from "./AIBuilder.module.scss";
 
-export const ChatPanel = () => {
+export const AIBuilder = () => {
     return (
         <div className={styles.container}>
             <Text size="md" bold>
                 AI Builder
             </Text>
-            <Text size="xl">WIP 🚧</Text>
-            <div className={styles.messages}>{/* Messages will go here */}</div>
+
+            <div className={styles.messages}>
+                <Text className={styles.tempMessage} size="xl">
+                    WIP 🚧
+                </Text>
+            </div>
             <div className={styles.input}>
                 <TextField
                     disabled
